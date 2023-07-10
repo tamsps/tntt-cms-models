42026622151620ce5fc192943e24e9db4cf45396
Tam Vu <tamsps@gmail.com>
2023-07-10 23:48:28 +0700

    update

---
+++ tntt-cms-models/Exception/ErrorDetails.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace TNTT.CMS.Models.Exception
+{
+    public class ErrorDetails
+    {
+        public int StatusCode { get; set; }
+        public string Message { get; set; }
+        public override string ToString()
+        {
+            return JsonSerializer.Serialize(this);
+        }
+    }
+}
